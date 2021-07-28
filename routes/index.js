1257fff5c71d711129f12b9126ec6ebfed312b3d
Luca1152 <t.luca1152@gmail.com>
2021-07-28 14:27:06 +0300

    Add messages to index
    Two messages were hardcoded and displayed on the index.html page.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+var express = require("express")
+var router = express.Router()
+
+const messages = [
+    {
+        text: "Salut, boss!",
+        user: "florin salam",
+        added: new Date(),
+    },
+    {
+        text: "Hallo!",
+        user: "nob",
+        added: new Date(),
+    },
+]
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get("/", function (req, res, next) {
+    res.render("index", { title: "Mini Messageboard", messages })
+})
 
-module.exports = router;
+module.exports = router
